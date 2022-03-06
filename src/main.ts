b95862b648ed87dfc4f183e02ca207e79542532f
bhu413 <bhu413@gmail.com>
2022-03-06 12:27:11 -0800

    Fixed package issues and removed template code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { Assignment, ButtonType } from "midi-mixer-plugin";
-const HomeAssistant = require('homeassistant');
 
 
+let HomeAssistant: any;
+try {
+  HomeAssistant = require('homeassistant');
+} catch (e) {
+  console.log(e);
+}
 /**
  * Welcome to the plugin's main file!
  *
@@ -14,11 +19,14 @@ const HomeAssistant = require('homeassistant');
  */
 
 
+
 let homeassistantURL = "";
 let authorizationToken = "";
+let refreshInterval: number;
+let refreshTimer: any;
 let hass: any;
-let assignments: Assignment[] = [];
-let buttons: ButtonType[] = [];
+let assignments = new Map<string, Assignment>();
+let buttons = new Map<string, ButtonType>();
 
 
 
@@ -26,19 +34,24 @@ let buttons: ButtonType[] = [];
  * Example of retrieving the plugin's settings.
  */
 $MM.getSettings().then((settings) => {
+
   console.log("Current settings:", settings);
   homeassistantURL = "http://" + settings.homeassistantIP;
   authorizationToken = settings.auth as string;
- 
-  /**
-  * Create the Homeassistant object
-  */
-  hass = new HomeAssistant({
-    host: homeassistantURL,
-    token: authorizationToken
-  })
+  refreshInterval = settings.refresh as number;
+  if (refreshInterval == null) {
+    refreshInterval = 0;
+  }
 
   try {
+    /**
+    * Create the Homeassistant object
+    */
+    hass = new HomeAssistant({
+      host: homeassistantURL,
+      token: authorizationToken
+    })
+
     hass.states.list().then((res: Array<any>) => {
 
       res.forEach(function (item: any) {
@@ -49,6 +62,7 @@ $MM.getSettings().then((settings) => {
           })
           // set action for mute event
           currentAssignment.on("mutePressed", async () => {
+            clearInterval(refreshTimer);
             let fullState = await hass.states.get("light", currentAssignment.id);
             let onOffState = fullState.state;
             if ((onOffState == "on") == !currentAssignment.muted) {
@@ -57,11 +71,14 @@ $MM.getSettings().then((settings) => {
             } else {
               currentAssignment.muted = !currentAssignment.muted;
             }
+            startRefreshTimer();
           })
 
           // set action for volume event
           currentAssignment.on("volumeChanged", (level: number) => {
+            clearInterval(refreshTimer);
             level = Math.round(level * 100);
+            //console.log(level);
             hass.services.call(hass.services.call('turn_on', 'light', {
               entity_id: currentAssignment.id,
               brightness_pct: level,
@@ -72,13 +89,14 @@ $MM.getSettings().then((settings) => {
             } else {
               currentAssignment.muted = false;
             }
+            startRefreshTimer();
           })
 
           // set delay in changes since homeassistant can't go fast
           currentAssignment.throttle = 350;
 
           // put it in the array
-          assignments.push(currentAssignment);
+          assignments.set(currentAssignment.id, currentAssignment);
 
         } else if (item.entity_id.startsWith("switch")) {
           let currentButton = new ButtonType(item.entity_id, {
@@ -87,6 +105,7 @@ $MM.getSettings().then((settings) => {
 
           // set action for pressed event
           currentButton.on("pressed", async () => {
+            clearInterval(refreshTimer);
             let fullState = await hass.states.get("switch", currentButton.id);
             let onOffState = fullState.state;
             if ((onOffState == "on") == currentButton.active) {
@@ -95,95 +114,48 @@ $MM.getSettings().then((settings) => {
             } else {
               currentButton.active = !currentButton.active;
             }
+            startRefreshTimer();
           })
 
           // put it in the array
-          buttons.push(currentButton);
+          buttons.set(currentButton.id, currentButton);
         }
       });
     });
-
+    refreshStates();
+    startRefreshTimer();
   } catch (e) {
     console.log(e);
   }
-  
 });
 
+function startRefreshTimer() {
+  if (refreshInterval != 0) {
+    refreshTimer = setInterval(refreshStates, refreshInterval * 1000);
+  }
+}
 
-/**
- * Example of setting up an assignment to be controlled by the plugin.
- */
-const example = new Assignment("foo", {
-  name: "Example Plugin Entry",
-});
-
-/**
- * Set the assignment's peak meter value to 50% for 150ms.
- */
-example.meter = 0.5;
-
-/**
- * Sets the minimum amount of time in milliseconds between volume change updates
- * from MIDI Mixer to 50 milliseconds.
- */
-example.throttle = 500;
-
-/**
- * When the user tries to change the volume of the assignment...
- */
-example.on("volumeChanged", (level: number) => {
-  /**
-   * Sets the volume indicator to the new level.
-   */
-  example.volume = level;
-});
-
-/**
- * When the user presses the "mute" button for this assignment...
- */
-example.on("mutePressed", async () => {
-  /**
-   * Toggles the "muted" indicator.
-   */
-  example.muted = !example.muted;
-});
-
-/**
- * When the user presses the "assign" button for this assignment...
- */
-example.on("assignPressed", () => {
-  /**
-   * Toggles the "assigned" indicator.
-   */
-  example.assigned = !example.assigned;
-});
-
-/**
- * When the user presses the "run" button for this assignment...
- */
-example.on("runPressed", () => {
-  /**
-   * Toggles the "running" indicator.
-   */
-  example.running = !example.running;
-});
-
-/**
- * Example of setting up a button type to be controlled by the plugin.
- */
-const typeExample = new ButtonType("bar", {
-  name: "Example Button Type",
-});
-
-/**
- * When the user presses the button...
- */
-typeExample.on("pressed", () => {
-  /**
-   * Toggles the button's indicator.
-   */
-  typeExample.active = !typeExample.active;
-});
+async function refreshStates() {
+  try {
+    let states = await hass.states.list();
+    console.log(states);
+    states.forEach(function (item: any) {
+      if (assignments.has(item.entity_id)) {
+        let currentAssignment = assignments.get(item.entity_id);
+        if (currentAssignment) {
+          currentAssignment.muted = item.state != "on";
+        }
+      } else if (buttons.has(item.entity_id)) {
+        let currentButton = buttons.get(item.entity_id);
+        if (currentButton) {
+          currentButton.active = item.state == "on";
+        }
+      }
+    })
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 /**
  * Example of settings buttons and statuses to show the status of the plugin
@@ -218,9 +190,7 @@ console.log(
 );
 
 console.log(`Welcome to the debugging tools for a MIDI Mixer plugin.
-
 Here are a few things to try out:
-
 - Watch this console for logs and errors
 - Set breakpoints in your code in the "Sources" tab
 - Profile memory usage using the "Memory" tab
